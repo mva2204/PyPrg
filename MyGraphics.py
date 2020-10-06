ab0f5db4ceb8c4c2aa4050843ba4dcb5f464e397
Вадим <mva2204@mail.ru>
2020-10-06 20:57:26 +0300

    Первая загрузка на Git

--- MyGraphics.py
+++ MyGraphics.py
@@ -2,17 +2,25 @@
 
 import matplotlib as mpl
 import math
+import numpy as np
 from matplotlib import pyplot as plt
 
 
 
 def plot_graph_smart():
     #Создание объекта Figure
-    fig = plt.figure()
+    x = np.arange(0,4*np.pi,0.1)
+    y = np.sin(x)
+    z = np.cos(x)
+    fig, axes = plt.subplots(nrows=2, ncols=1)
+#    fig = plt.figure()
     #Тип объекта Figure
     print(type(fig))
     #scatter - метод для нанесения маркера в точке (1.0, 1.0)
-    plt.scatter(1.0, 1.0)
+
+    axes[0].plot(x, y, x,z)
+#    plt.scatter(1.0, 1.0)
+    axes[1].scatter(1.0, 5.0)
     print(fig.axes)
     #plt.show()
     #fig, axes = plt.subplots
